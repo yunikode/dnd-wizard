f8dadb400db9233c244c2715a3fa3f42928611ac
zee-german <franz.lutzenberger@gmail.com>
2019-03-01 08:50:27 +0100

    added primalreact ui and clean of boilerplate app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 import * as React from 'react';
 import axios from 'axios'
-import logo from './logo.svg';
-import './App.scss';
 
 export class App extends React.Component {
   async componentDidMount() {
     const {data} = await axios.get('/spells/1')
     console.log(data)
-  } 
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h1 className="app-header">D&D Wizard</h1>
+        <main className="app-container">
+          <article className="wizard">wizard works</article>
+        </main>
+      </>
     );
   }
 }
